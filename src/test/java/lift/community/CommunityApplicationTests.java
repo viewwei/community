b2b8919fc7cf6b16d17f112aa4e734ba368ed42a
邹威 <805822426@qq.com>
2019-11-30 15:18:47 +0800

    init repo

---
+++ src/test/java/lift/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package lift.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
